5afacf4047fda319ff657799f18e32c0b8f5ed5e
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-09 16:17:39 -0700

    Completed the required mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,35 +24,32 @@ export const ADD_USER = gql`
   }
 `;
 
-// TODO: Add ADD_BOOK and REMOVE_BOOK
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookContent!) {
+    saveBook(book: $book) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
 
-// export const ADD_THOUGHT = gql`
-//   mutation addThought($thoughtText: String!) {
-//     addThought(thoughtText: $thoughtText) {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//       comments {
-//         _id
-//         commentText
-//       }
-//     }
-//   }
-// `;
-
-// export const ADD_COMMENT = gql`
-//   mutation addComment($thoughtId: ID!, $commentText: String!) {
-//     addComment(thoughtId: $thoughtId, commentText: $commentText) {
-//       _id
-//       thoughtText
-//       thoughtAuthor
-//       createdAt
-//       comments {
-//         _id
-//         commentText
-//         createdAt
-//       }
-//     }
-//   }
-// `;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`;
