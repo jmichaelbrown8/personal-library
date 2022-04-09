f6371a6c23583d54c1c8319cf8200b626e14d3dc
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-08 21:25:35 -0700

    Got addUser mutation and me query working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,20 +3,18 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
-    email: String
-    password: String
-    savedBooks: [Book]!
+    username: String!
+    email: String!
+    savedBooks: [Book]
   }
 
   type Book {
-    _id: ID
+    bookId: String!
     authors: [String]
     description: String!
-    bookId: String!
+    title: String!
     image: String
     link: String
-    title: String!
   }
 
   type Auth {
@@ -25,19 +23,15 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    books(username: String): [Book]
-    book(bookId: ID!): Book
     me: User
   }
 
-  #   type Mutation {
-  #     addUser(username: String!, email: String!, password: String!): Auth
-  #     login(email: String!, password: String!): Auth
-  #     saveBook(book: Book!): Book
-  #     removeBook(bookId: ID!): Book
-  #   }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    #     saveBook(book: Book!): User
+    #     removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
 const path = require("path");
-const db = require("./config/connection");
-const authMiddleware = require("./utils/auth");
+const { authMiddleware } = require("./utils/auth");
 
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
