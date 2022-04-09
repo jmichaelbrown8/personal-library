b0dd8e7b83fd43a1303475a3cec709a326d1b704
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-09 13:01:08 -0700

    Converted over the login and signup API calls to gql mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
+// TODO: Change getMe and deleteBook to gql
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
